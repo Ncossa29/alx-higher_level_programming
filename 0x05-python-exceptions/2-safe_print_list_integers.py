d2fd250e92babc060ac9bd426b6a5d11fbe32ca0
Ncossa29 <nyeleti.strela@gmail.com>
2024-04-15 07:47:56 -0700

    task2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    c = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            num += 1
+            for x in range(c):
+                print("", end="")
+        except (ValueError, TypeError):
+            print("", end="")
+    print()
+    return c
